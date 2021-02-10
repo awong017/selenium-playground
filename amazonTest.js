d05ea235c035aa55cd72163dae7fc2b79bb02c37
Adam Wong <adamwthedev@gmail.com>
2021-02-09 22:47:34 -0800

    created amazonTest.js

---
+++ amazonTest.js
@@ -0,0 +1,11 @@
+const { Builder, By, Key, Until} = require("selenium-webdriver");
+require("chromedriver");
+
+async function amazonTest() {
+    let driver = new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.amazon.com");
+    await driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Manscape", Key.RETURN);
+    await driver.close();
+};
+
+amazonTest();
--- youTubeTest.js
+++ youTubeTest.js
@@ -1,10 +1,12 @@
-const { Builder, By } = require("selenium-webdriver");
+const { Builder, By, Key, Until } = require("selenium-webdriver");
 require("chromedriver");
 
 async function youTubeTest() {
     let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("http://www.google.com");
-    await driver.findElement(By.name("q")).sendKeys("Donut", Key.RETURN);
+    await driver.get("https://www.youtube.com");
+    await driver.findElement(By.name("search_query")).sendKeys("Donut Media", Key.RETURN);
+    await driver.findElement(By.id("avatar")).click();
+    await driver.close();
 };
 
 youTubeTest();
