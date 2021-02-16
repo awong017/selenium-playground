df8a4c94d3140502d8822ea24d7f691ea1df3226
Adam Wong <adamwthedev@gmail.com>
2021-02-15 19:42:52 -0800

    changed smashTest and facebookTest

--- facebookTest.js
+++ facebookTest.js
@@ -4,8 +4,8 @@ require("chromedriver");
 async function facebookTest() {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("https://www.facebook.com");
-    await driver.findElement(By.id("email")).sendKeys("spitfire@aol.com");
-    await driver.findElement(By.id("pass")).sendKeys("notapassword");
+    await driver.findElement(By.id("email")).sendKeys("na");
+    await driver.findElement(By.id("pass")).sendKeys("na");
     await driver.quit();
 };
 
--- smashTest.js
+++ smashTest.js
@@ -5,8 +5,8 @@ async function smashLogin() {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("https://smash-ult-stats-tracker-aruj0k060.vercel.app/");
     await driver.findElement(By.linkText("Login")).click();
-    await driver.findElement(By.className("name-input")).sendKeys("awong017");
-    await driver.findElement(By.className("password-input")).sendKeys("awong017");
+    await driver.findElement(By.className("name-input")).sendKeys("na");
+    await driver.findElement(By.className("password-input")).sendKeys("na");
     await driver.findElement(By.className("login-button")).click();
 }
 smashLogin();
