7b4da2c2ddd6ab4cc120f2551d4d2de071b5b43c
Adam Wong <adamwthedev@gmail.com>
2021-02-23 19:28:50 -0800

    created leetcodeTest.js

---
+++ leetcodeTest.js
@@ -0,0 +1,10 @@
+const { Builder, By, Keys, Until } = require("selenium-webdriver");
+require("chromedriver");
+
+async function leetcodeTest() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.leetcode.com");
+    await driver.quit();
+};
+
+leetcodeTest();
